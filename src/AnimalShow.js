fcb3a06f79bee41c24c0f633d29f543ac2f2f28d
Noi-Git <noi.patsin@gmail.com>
2022-12-07 22:14:57 -0500

    add animal image compare with type

--- src/AnimalShow.js
+++ src/AnimalShow.js
@@ -1,7 +1,20 @@
-import React from 'react'
+import React, { useState } from 'react'
+import bird from './svg/bird.svg'
+import cat from './svg/cat.svg'
+import cow from './svg/cow.svg'
+import dog from './svg/dog.svg'
+import gator from './svg/gator.svg'
+import heart from './svg/heart.svg'
+import horse from './svg/horse.svg'
 
-const AnimalShow = () => {
-  return <div>AnimalShow</div>
+const svgMap = { bird, cat, cow, dog, gator, horse }
+
+const AnimalShow = ({ type }) => {
+  return (
+    <div>
+      <img src={svgMap[type]} alt={type} />
+    </div>
+  )
 }
 
 export default AnimalShow
