cfdd1f3bd2ae93528a7f71b9ef7b373d59a37679
Noi-Git <noi.patsin@gmail.com>
2022-12-07 21:39:46 -0500

    add random animal when clicked

--- src/App.js
+++ src/App.js
@@ -1,5 +1,26 @@
+import { useState } from 'react'
+import AnimalShow from './AnimalShow'
+
+const getRandomAnimal = () => {
+  const animals = ['bird', 'cat', 'cow', 'dog', 'gator', 'horse']
+
+  return animals[Math.floor(Math.random() * animals.length)]
+}
+
 function App() {
-  return <div>Test</div>
+  const [animals, setAnimals] = useState([])
+
+  const handleClick = () => {
+    setAnimals(getRandomAnimal())
+  }
+
+  return (
+    <div>
+      <AnimalShow />
+      <button onClick={handleClick}>Add Animal</button>
+      <div>{animals}</div>
+    </div>
+  )
 }
 
 export default App
